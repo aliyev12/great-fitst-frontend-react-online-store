edaf75b3898a8a7951ffee0a03d54482b73a1776
aliyev12 <aaa719@nyu.edu>
2019-03-06 06:34:24 -0500

    adding styled components to Page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d22fd60f328575bd459";
+/******/ 	var hotCurrentHash = "8d1d012feaa7430ba6b8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
