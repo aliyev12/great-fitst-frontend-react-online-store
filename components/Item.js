4a9daaf484116ec93e65067027a546b8434c5b05
aliyev12 <aaa719@nyu.edu>
2019-03-12 22:40:28 -0400

    synking

--- components/Item.js
+++ components/Item.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
-import PriceTag
+// import PriceTag
 
 export default class Item extends Component {
   static propTypes = {
